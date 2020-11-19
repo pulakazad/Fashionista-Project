38302e2973e91351659b4b75bd570187626eb8a9
pulakazad <pulakazad@gatech.edu>
2020-11-18 22:25:05 -0500

    added fashionista folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddCompanyComponent } from './add-company/add-company.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AuthGuard } from './auth/auth.guard';
+import { CartComponent } from './cart/cart.component';
+import { CompaniesListComponent } from './companies-list/companies-list.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { UpdateCompanyComponent } from './update-company/update-company.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'companies-list', component: CompaniesListComponent, canActivate: [AuthGuard] },
+  { path: 'add-company', component: AddCompanyComponent, canActivate: [AuthGuard] },
+  { path: 'update-company', component: UpdateCompanyComponent, canActivate: [AuthGuard] },
+  { path: 'products-list', component: ProductsListComponent, canActivate: [AuthGuard] },
+  { path: 'add-product', component: AddProductComponent, canActivate: [AuthGuard] },
+  { path: 'update-product', component: UpdateProductComponent, canActivate: [AuthGuard] },
+  { path: 'view-products', component: ViewProductsComponent},
+  { path: 'view-cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AuthComponent } from './auth/auth.component';
+// import { LoginComponent } from './auth/login/login.component';
+// import { RegisterComponent } from './auth/register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthModule } from './auth/auth.module';
+import { AddCompanyComponent } from './add-company/add-company.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CompaniesListComponent } from './companies-list/companies-list.component';
+import { UpdateCompanyComponent } from './update-company/update-company.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddCompanyComponent,
+    CompaniesListComponent,
+    UpdateCompanyComponent,
+    ProductsListComponent,
+    AddProductComponent,
+    UpdateProductComponent,
+    ViewProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,  
+    AuthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
